80e360d2e6572de531d316e5c39510c1fdd71a5e
ivet21-netizen <ivetgalmed21@gmail.com>
2025-11-25 17:00:52 -0600

    Create manejo integral de residuos.py

---
+++ manejo integral de residuos.py
@@ -0,0 +1,10 @@
+organico= "hojas, cascaras_de_fruta, restos_alimentos" 
+inorganico_no_reciclable="empaques, servilletas, envolturas"
+inorganico_reciclable="carton, papel"
+pet_y_aluminio="envases, latas, botellas"
+print("tiene hojas", "hojas" in organico)
+print("¿tiene papel?", "papel" in inorganico_reciclable)
+print("¿tiene baterias?", "baterias" in organico)
+      
+      
+      
